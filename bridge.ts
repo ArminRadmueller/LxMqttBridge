1bf6333a45e273ec034d476bc0a0cc00c2fed9dc
Armin Radmüller <Armin.Radmueller@live.com>
2021-12-28 01:15:03 +0100

    update package.json + git ignore

--- bridge.ts
+++ bridge.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const logging = require('yalm')
-const program = require('./package.json')
 const config = require(process.argv[2] || './config.json')
 
 import {bridgeToLoxone} from "./bridgeLoxone"
@@ -9,7 +8,7 @@ import {bridgeToMqttBroker} from "./bridgeMqttBroker"
 
 logging.setLevel(config.logLevel)
 
-logging.info('Loxone-MQTT Bridge (LxMqttBridge) ' + program.version + ' starting...')
+logging.info('Loxone-MQTT Bridge (LxMqttBridge) starting...')
 
 var bdgLx = new bridgeToLoxone(config.loxone.host, config.loxone.port);
 bdgLx.on('receiveFromLoxone', (message:string) => { processIncomingLoxoneMessage(message) } );
